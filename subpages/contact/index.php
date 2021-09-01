aefb0450f37af03b0b2af6be6b7c42d87e544474
milkrz <88857062+milkrz@users.noreply.github.com>
2021-09-01 16:25:45 +0200

    Update index.php

--- subpages/contact/index.php
+++ subpages/contact/index.php
@@ -14,7 +14,7 @@ if (!isset($page)) {
         <div class="col-md-6 col-12 align-self-center">
             <div class="mapouter">
                 <div class="gmap_canvas border">
-                    <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=OS. STAWKI 17/11&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                    <iframe class="gmap_iframe" width="100%" frameborder="3" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=OS. STAWKI 17/11&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                 </div>
                 <style>
                     .mapouter {
@@ -46,14 +46,14 @@ if (!isset($page)) {
                 </p>
             </div>
             <div>
-            <i class="bi bi-geo-alt-fill me-2"></i> <strong>Adres :</strong>
+            <i class="bi bi-geo-alt-fill me-2"></i><strong>Adres :</strong>
 			<i><a href="google.navigation:q=50.95028348400445, 21.41137318479233&mode=d"><p class="no_spacing">Osiedle Stawki 17/11</p>
                 <p class="no_spacing">27-400 Ostrowiec Świętokrzyski</p></i></a>
                 
             </div>
 
             <div class="mt-3">
-                <i class="bi bi-envelope-fill me-2"></i> <strong>E-Mail :</strong>
+                <i class="bi bi-envelope-fill me-2"></i><strong>E-mail :</strong>
                 <p><i class="no_spacing"><a href="mailto:kontakt@goodhao.pl" class="no_spacing" >kontakt@goodhao.pl</a></i></p>
             </div>
 
@@ -63,12 +63,14 @@ if (!isset($page)) {
             </div>
 
             <div class="mt-3">
-            <i class="bi bi-door-open-fill me-2"></i></i><strong>Godziny otwarcia :</strong>
-                <p class="no_spacing">Pn – Pt w godz. 7.00 – 21.00</p>
+            <i class="bi bi-door-open-fill me-2"></i><strong>Godziny otwarcia :</strong>
+                <i><p class="no_spacing">poniedziałek - piątek: 9:00 - 21:00</p></i>
+				<i><p class="no_spacing">sobota: nieczynne</p><i>
             </div>
 
             <div class="mt-3">
-                <p><strong>NIP:</strong> 6611227724</p>
+			<i class="bi bi-bank2"><strong>   NIP :</strong></i>  <i class="no_spacing">6611227724</i>
+               
             </div>
         </div>
     </div>
